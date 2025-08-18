0409431a5e85b830259b255734b5e85e18da2c69
datamoshAngel <angeloressilva@gmail.com>
2025-08-17 23:51:20 -0300

    Add files via upload

--- tp.c
+++ tp.c
@@ -2,11 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ANSI_RESET            "\x1b[0m"
+#define ANSI_BOLD             "\x1b[1m"
+#define ANSI_BG_COLOR_RED     "\x1b[41m"
+
+#define BOLD(string)       ANSI_BOLD             string ANSI_RESET
+#define BG_RED(string)     ANSI_BG_COLOR_RED     string ANSI_RESET
+
 void limparBuffer();
 void letraMaiuscula(char *comando);
 void invalido();
 void centralizado(char *texto);
 void quebrar(int n);
+void margem(int n);
 
 int main() {
   int jogo = 1;
@@ -14,19 +22,21 @@ int main() {
     char comando;
     
     system("clear");
-    printf("\n\n\n");
-    centralizado("2048!\n\n");
-    centralizado("by Angelo Resende\n\n\n\n");
-    centralizado("O que deseja fazer?\n\n\n");
-    centralizado("(N) Novo jogo\n\n");
-    centralizado("(J) Continuar jogo atual\n\n");
-    centralizado("(C) Carregar um jogo salvo\n\n");
-    centralizado("(S) Salvar o jogo atual\n\n");
-    centralizado("(M) Mostrar Ranking\n\n");
-    centralizado("(A) Ajuda com as instruções de como jogar\n\n");
-    centralizado("(R) Sair\n\n");
+    quebrar(4);
+    centralizado(BG_RED(BOLD("2048!")) "\n\n\n");
+    centralizado("by Angelo Resende\n\n");
+    quebrar(3);
+    centralizado("O que deseja fazer?\n\n");
+    centralizado(BOLD("(N)") " Novo jogo\n");
+    centralizado(BOLD("(J)") " Continuar jogo atual\n");
+    centralizado(BOLD("(C)") " Carregar um jogo salvo\n");
+    centralizado(BOLD("(S)") " Salvar o jogo atual\n");
+    centralizado(BOLD("(M)") " Mostrar Ranking\n");
+    centralizado(BOLD("(A)") " Ajuda com as instruções de como jogar\n");
+    centralizado(BOLD("(R)") " Sair");
     
-    printf("\n\n\n\n\n\tComando: ");
+    quebrar(11);
+    printf("\t" BOLD("Comando:") " ");
     scanf(" %c", &comando);
     limparBuffer();
     letraMaiuscula(&comando);
@@ -50,16 +60,27 @@ int main() {
       case 'A':
         while (1 != 0) {
           system("clear");
-          printf("\n\n\t\t\t\t2048 é um jogo de raciocínio onde o objetivo é deslizar peças numeradas em um tabuleiro e combiná-las até formar um\n"); 
-          printf("\t\t\t\tbloco com o número 2048.\n");
-          printf("\n\n\t\t\t\t\t- O jogador desliza as peças em um dos quatro sentidos (cima, baixo, esquerda e direita)\n");
-          printf("\n\t\t\t\t\t- A cada movimento, um novo número aparece aleatoriamente em um local vazio do tabuleiro.\n");
-          printf("\n\t\t\t\t\t- As peças deslizam o mais longe possível até colidirem.\n");
-          printf("\n\t\t\t\t\t- Duas peças iguais que se colidirem irão se fundir em uma peça com o valor total das duas peças.\n");
-          printf("\n\n\t\t\t\tEsse é o 2048.\n\n");
+          quebrar(2);
+          margem(4);
+          printf("2048 é um jogo de raciocínio onde o objetivo é deslizar peças numeradas em um tabuleiro e combiná-las até formar um\n"); 
+          margem(4);
+          printf("bloco com o número 2048.\n\n\n");
+          margem(5);
+          printf("- O jogador desliza as peças em um dos quatro sentidos (cima, baixo, esquerda e direita)\n\n");
+          margem(5);
+          printf("- A cada movimento, um novo número aparece aleatoriamente em um local vazio do tabuleiro.\n\n");
+          margem(5);
+          printf("- As peças deslizam o mais longe possível até colidirem.\n\n");
+          margem(5);
+          printf("- Duas peças iguais que se colidirem irão se fundir em uma peça com o valor total das duas peças.\n\n\n");
+          margem(4);
+          printf("Esse é o 2048.");
           
-          centralizado("(R) Voltar ao menu\n\n\tComando: ");
+          quebrar(5);
+          centralizado(BOLD("(R)") " Voltar ao menu");
+          quebrar(20);
           
+          printf("\t" BOLD("Comando:") " ");
           char comando2;
           scanf(" %c", &comando2);
           limparBuffer();
@@ -76,10 +97,10 @@ int main() {
           system("clear");
           quebrar(19);
           centralizado("Tem certeza que deseja sair?");
-          centralizado("(S) Sim");
-          centralizado("(N) Nao");
-          quebrar(19);
-          printf("\tComando: ");
+          centralizado(BOLD("(S)") " Sim");
+          centralizado(BOLD("(N)") " Não");
+          quebrar(19); 
+          printf("\t" BOLD("Comando:") " ");
           
           char confirma;
           scanf(" %c", &confirma);
@@ -137,18 +158,24 @@ void centralizado(char *texto) {
   for (int i = 0; i < tamTxt; i++)
     if (texto[i] == '\n')
       quebra += 1;
-    else if (texto[i] == '´' || 
+    else if (texto[i] == '´' || texto[i] == '`' || texto[i] == '~' || texto[i] == '^')
       acentos += 1;
   
   // Tamanho de espaços a serem colocados à esquerda do texto
   n = (184 - (tamTxt - quebra - acentos)) / 2;
   
   // Imprime o texto adicionando n espaços à esquerda
-  printf("%*s", (int)(n + tamTxt), texto);
+  printf("%*s\n", (int)(n + tamTxt), texto);
 }
 
 // Quebra n linhas
 void quebrar(int n) {
   for (int i = 0; i < n; i++)
     printf("\n");
+}
+
+// Adiciona '\t' n vezes
+void margem(int n) {
+  for (int i = 0; i < n; i++)
+    printf("\t");
 }
